6493d552ab9c7af76b3ad87ac9a6bc042b67b935
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-04-23 11:25:32 +0800

    adding rectangular facial layers

--- facedetector.py
+++ facedetector.py
@@ -16,11 +16,15 @@ faces=face_cascade.detectMultiScale(gray_img,
 scaleFactor=1.05,
 minNeighbors=5)
 
+#for loop to create a rectangular layer
+for x, y, w, h in faces:
+    img=cv2.rectangle(img, (x,y),(x+w,+y+h), (0,255,0),3)
+
 
 print(type(faces))
 print(faces)
 
 
-cv2.imshow("Gray", gray_img)
+cv2.imshow("Gray",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
